5cd035914bf3ceb29e61c6497cd382a89446afd9
matheus assis <masj@cesar.school>
2026-09-09 19:33:37 -0300

    rate

--- main.c
+++ main.c
@@ -43,6 +43,25 @@ void liberarLista(Tarefa *inicio) {
     }
 }
 
+Tarefa *escolherRate(Tarefa *inicio) {
+    Tarefa *escolhida = NULL;
+    Tarefa *atual = inicio;
+
+    while (atual != NULL) {
+
+        if (atual->tempo_restante > 0) {
+
+            if (escolhida == NULL || atual->periodo < escolhida->periodo) {
+                escolhida = atual;
+            }
+        }
+
+        atual = atual->prox;
+    }
+
+    return escolhida;
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
@@ -130,6 +149,20 @@ int main(int argc, char *argv[]) {
 
     fclose(arquivo);
 
+    FILE *saida = fopen("rate_masj.out", "w");
+
+    if (saida == NULL) {
+        fprintf(stderr, "Erro: nao foi possivel criar o arquivo de saida.\n");
+        liberarLista(inicio);
+        return 1;
+    }
+
+    fprintf(saida, "EXECUTION BY RATE\n");
+
+    Tarefa *anterior = NULL;
+    int unidades_bloco = 0;
+    int unidades_idle = 0;
+
     for (int tempo = 0; tempo < tempo_total; tempo++) {
 
         Tarefa *atual = inicio;
@@ -137,6 +170,14 @@ int main(int argc, char *argv[]) {
         while (atual != NULL) {
 
             if (atual->tempo_restante > 0 && tempo == atual->deadline_absoluto) {
+
+                if (atual == anterior && unidades_bloco > 0) {
+                    fprintf(saida, "[%s] for %d units - L\n",
+                            atual->nome, unidades_bloco);
+
+                    anterior = NULL;
+                    unidades_bloco = 0;
+                }
 
                 atual->perdidas++;
                 atual->tempo_restante = 0;
@@ -151,17 +192,83 @@ int main(int argc, char *argv[]) {
 
             atual = atual->prox;
         }
-    }
 
+        if (strcmp(argv[1], "rate") == 0) {
+            Tarefa *executando = escolherRate(inicio);
+
+            if (executando != NULL) {
+
+                if (unidades_idle > 0) {
+                    fprintf(saida, "idle for %d units\n", unidades_idle);
+                    unidades_idle = 0;
+                }
+
+                if (anterior == NULL) {
+
+                    anterior = executando;
+                    unidades_bloco = 0;
+
+                } else if (anterior != executando) {
+
+                    fprintf(saida, "[%s] for %d units - H\n",
+                            anterior->nome, unidades_bloco);
+
+                    anterior = executando;
+                    unidades_bloco = 0;
+                }
+
+                unidades_bloco++;
+
+                executando->tempo_restante--;
+
+                if (executando->tempo_restante == 0) {
+
+                    executando->completas++;
+
+                    fprintf(saida, "[%s] for %d units - F\n",
+                            executando->nome, unidades_bloco);
+
+                    anterior = NULL;
+                    unidades_bloco = 0;
+                }
+
+            } else {
+
+                unidades_idle++;
+            }
+
+        }
+    }
+    if (unidades_idle > 0) {
+        fprintf(saida, "idle for %d units\n", unidades_idle);
+    }
     Tarefa *atual = inicio;
 
     while (atual != NULL) {
-
         if (atual->tempo_restante > 0) {
 
             if (atual->deadline_absoluto == tempo_total) {
+
+                if (atual == anterior && unidades_bloco > 0) {
+                    fprintf(saida, "[%s] for %d units - L\n",
+                            atual->nome, unidades_bloco);
+
+                    anterior = NULL;
+                    unidades_bloco = 0;
+                }
+
                 atual->perdidas++;
+
             } else {
+
+                if (atual == anterior && unidades_bloco > 0) {
+                    fprintf(saida, "[%s] for %d units - K\n",
+                            atual->nome, unidades_bloco);
+
+                    anterior = NULL;
+                    unidades_bloco = 0;
+                }
+
                 atual->killed++;
             }
 
@@ -170,7 +277,31 @@ int main(int argc, char *argv[]) {
 
         atual = atual->prox;
     }
+    fprintf(saida, "LOST DEADLINES\n");
+    atual = inicio;
+    while (atual != NULL) {
+        fprintf(saida, "[%s] %d\n", atual->nome, atual->perdidas);
+        atual = atual->prox;
+    }
 
+    fprintf(saida, "COMPLETE EXECUTION\n");
+
+    atual = inicio;
+
+    while (atual != NULL) {
+        fprintf(saida, "[%s] %d\n", atual->nome, atual->completas);
+        atual = atual->prox;
+    }
+
+    fprintf(saida, "KILLED\n");
+
+    atual = inicio;
+
+    while (atual != NULL) {
+        fprintf(saida, "[%s] %d\n", atual->nome, atual->killed);
+        atual = atual->prox;
+    }
+    fclose(saida);
     liberarLista(inicio);
     return 0;
 }
